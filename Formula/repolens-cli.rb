41daf670c38a8122f73a6eb10fe16eb53da3f6de
Satyam singh <106249269+Satyam12singh@users.noreply.github.com>
2026-06-14 23:04:53 +0530

    Update repolens-cli to v0.2.1

--- Formula/repolens-cli.rb
+++ Formula/repolens-cli.rb
@@ -1,10 +1,10 @@
 class RepolensCli < Formula
   desc "Instant clarity on any codebase — navigate imports, trace functions, and get AI answers in one terminal view"
   homepage "https://github.com/Satyam12singh/repolens"
-  url "https://files.pythonhosted.org/packages/83/b3/7d8e13d36072da095df9e42bfca7a1ecc2245390b8b29bf4503a845fef88/repolens_cli-0.2.0.tar.gz"
-  sha256 "723cba65b564cf565db60f43f814b7d35a83921b63727f76073e4a62fd141f28"
+  url "https://files.pythonhosted.org/packages/24/c9/07b495143cabcb190f1c7cd1df870f21a4cb48a630942aef1c7f83e68728/repolens_cli-0.2.1.tar.gz"
+  sha256 "e3f443bd7362372cf3a808ca2727d6d3514621e63484015510bd7bdb1fd422e5"
   license "MIT"
-  version "0.2.0"
+  version "0.2.1"
 
   depends_on "python@3.12"
 
